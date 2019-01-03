8b47563856069dec2ace31f0891162c6c5e163ba
hawkeye116477 <hawkeye116477@gmail.com>
2019-01-03 11:11:04 +0100

    Add new script [ci skip]

---
+++ scripts/VICHS_auto.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Sciezka to miejsce, w którym znajduje się skrypt
+sciezka=$(dirname "$0")
+
+cd $sciezka/..
+
+if [ "$CI" = "true" ] ; then
+    git config --global user.email "PolishJarvis@int.pl"
+    git config --global user.name "PolishJarvis"
+    ost_plik=$(git diff-tree --no-commit-id --name-only -r master)
+else
+    ost_plik=$(git diff -z --name-only | xargs -0)
+fi
+
+
+if [[ "$ost_plik" == "polish_rss_filters"* ]]; then
+    if [[ "$lista" != *" polish_rss_filters.txt"* ]] ;then
+        lista+=" "polish_rss_filters.txt
+    fi
+if
+
+$sciezka/VICHS.sh $lista
