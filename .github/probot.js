493830bf579d9f6fb029e5a36c5eeb403ee2e11d
krystian3w <35370833+krystian3w@users.noreply.github.com>
2020-01-25 07:32:29 +0100

    Update probot.js

--- .github/probot.js
+++ .github/probot.js
@@ -6,10 +6,12 @@ on('issues.labeled')
   .filter(context => context.payload.label.name === 'odrzucone')
   .close();
 
-// on('issues.opened')
-//  .filter(context => !context.payload.issue.body.match(/### Typ elementu/) || context.payload.issue.body.includes('- [ ]'))
-//  .comment(contents('.github/MISSING_ISSUE_TEMPLATE_AUTOREPLY.md'))
-//  .close();
+/*
+on('issues.opened')
+  .filter(context => !context.payload.issue.body.match(/### Typ elementu/) || context.payload.issue.body.includes('- [ ]'))
+  .comment(contents('.github/MISSING_ISSUE_TEMPLATE_AUTOREPLY.md'))
+  .close();
+*/
 
 
 on('issues.opened', 'issues.edited')
